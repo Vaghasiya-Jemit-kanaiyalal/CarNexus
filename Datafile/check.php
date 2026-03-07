a8c66c0ecb515ca522562a036412bfbb68988b96
unknown <jemitvaghasiya07@gmail.com>
2026-03-07 10:01:12 +0530

    removed datafiles(testing data)

--- Datafile/check.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo("<h1>Check.php page</h1>\n");
-echo("PHP is working fine on this server!\n");
-?>
--- Datafile/fail.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo("<h1>Fail.php page</h1>\n");
-echo("This is just a test failure page.\n");
-?>
